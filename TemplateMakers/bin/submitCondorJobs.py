d26943919bdcec31cb5c5368c2fa1a6d1c31d826
Jason Michael Slaunwhite <jason.michael.slaunwhite@cern.ch>
2012-07-10 10:41:20 +0000

    minor updates

--- TemplateMakers/bin/submitCondorJobs.py
+++ TemplateMakers/bin/submitCondorJobs.py
@@ -2,16 +2,68 @@
 
 import os
 import sys
-
+import time
 
 
 def main ():
 
 
 
-	listOfSamples = ['ttH130', 'ttH120', 'ttH110']
+
+
+
+	listOfSamples = ['DoubleElectron_Run2011A-05Aug2011-v1', 
+					 'DoubleElectron_Run2011A-May10ReReco-v1',
+					 'DoubleElectron_Run2011A-PromptReco-v4',
+					 'DoubleElectron_Run2011A-PromptReco-v6',
+					 'DoubleElectron_Run2011B-PromptReco-v1',
+					 'DoubleMu_Run2011A-05Aug2011-v1',
+					 'DoubleMu_Run2011A-May10ReReco-v1',
+					 'DoubleMu_Run2011A-PromptReco-v4',
+					 'DoubleMu_Run2011A-PromptReco-v6',
+					 'DoubleMu_Run2011B-PromptReco-v1',
+					 'MuEG_Run2011A-05Aug2011-v1',
+					 'MuEG_Run2011A-May10ReReco-v1',
+					 'MuEG_Run2011A-PromptReco-v4',
+					 'MuEG_Run2011A-PromptReco-v6',
+					 'MuEG_Run2011B-PromptReco-v1',
+					 'scaledown_ttbar',
+					 'scaledown_ttbar_bb',
+					 'scaledown_ttbar_cc',
+					 'scaleup_ttbar',
+					 'scaleup_ttbar_bb',
+					 'scaleup_ttbar_cc',
+					 'singlet_s',
+					 'singlet_t',
+					 'singlet_tW',
+					 'singletbar_s',
+					 'singletbar_t',
+					 'singletbar_tW',
+					 'ttH100',
+					 'ttH105',
+					 'ttH110',
+					 'ttH115',
+					 'ttH120',
+					 'ttH125',
+					 'ttH130',
+					 'ttH135',
+					 'ttH140',
+					 'ttbar',
+					 'ttbarW',
+					 'ttbarZ',
+					 'ttbar_bb',
+					 'ttbar_cc',
+					 'wjets',
+					 'ww',
+					 'wz',
+					 'zjets',
+					 'zz']
+
+
+
+		
 	jesChoice = 0
-	jobLabel = "testCondor"
+	jobLabel = "betterNamesV1"
 
 	for iList in listOfSamples:
 		condorHeader = "universe = vanilla\n"+"executable = runTemplatesCondor_modDilep.csh\n"+"notification = Never\n"+"log = batchBEAN/templates_modDilep_newSample.logfile\n"+"getenv = True\n"
@@ -47,6 +99,8 @@ def main ():
 		condorJobFile.close()
 		print "Trying to submit jobs..."
 		print os.popen("condor_submit dilBatch.submit").readlines()
+		print "Now sleeping for a little..."
+		time.sleep(5)
 
 	print "Done with loop over samples"
 	
