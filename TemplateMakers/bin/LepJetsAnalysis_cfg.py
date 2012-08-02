9305f486ec0ccbabaf554a3dc6aedcbd11a5cf14
Tessa Pearson <tessa.jae.pearson@cern.ch>
2012-08-02 14:52:11 +0000

    2012 Pileup dist

--- TemplateMakers/bin/LepJetsAnalysis_cfg.py
+++ TemplateMakers/bin/LepJetsAnalysis_cfg.py
@@ -16,15 +16,16 @@ print "Inside ttbar Lepton+Jets config"
 
 process.inputs = cms.PSet (
     fileNames = cms.vstring(
+	'file:/store/user/tpearson/SingleMu/NovaBeanSkim_TopSel_525_2012B_IsoMu24_v1/c64307331babd3090ac687e7b46c1890/NOVa_Skim_ttbar_7_1_kP3.root'
 	#'file:~/Data/BEANS/pat_52x_test_300_1_U6n.root' #data BEAN
-	'file:~/Data/BEANS/pat_52x_test_94_1_roM.root' #wjets BEAN
+	#'file:~/Data/BEANS/pat_52x_test_94_1_roM.root' #wjets BEAN
 	  ###'file:SynchSkim_3jetCut.root'
     ),
-	maxEvents = cms.int32(-1)
+	maxEvents = cms.int32(100)
 )
 
 process.outputs = cms.PSet (
-    outputName = cms.string('wjets_test.root')
+    outputName = cms.string('test.root')
 )
 
 
@@ -32,11 +33,12 @@ process.dilAnalysis = cms.PSet(
 	jes = cms.int32(0),
 	jer = cms.int32(0),
 	btagFile = cms.FileInPath("mc_btag_efficiency_v4_histo.root"),
-	puFile = cms.FileInPath("collect_pileup_histos_v1_histo.root"),
-	sampleName = cms.string("wjets"),
+	#puFile = cms.FileInPath("collect_pileup_histos_v1_histo.root"),
+	puFile = cms.FileInPath("2012PileUpDists_LepJets.root"),
+	sampleName = cms.string("SingleMu"),
 	eleSel = cms.bool(True), #If no skim was applied 
 	muonSel  = cms.bool(True), #If no skim was applied TightMu and LooseLepVetos
 	jetSel = cms.bool(True),  #If skim was not applied to >=3jets
-	minJets= cms.int32(4), #minimum number of jets
+	minJets= cms.int32(0), #minimum number of jets
 	btags = cms.double(-1), #-1: pretag, 0: 0tag, n: >=ntags
 )
