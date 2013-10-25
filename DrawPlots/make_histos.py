c07ceaab28cb6be248f3d6c64331afcbb38b10a9
annawoodard <awoodard@nd.edu>
2013-10-25 12:57:35 -0400

    Reorganizing: moving plot making up one directory, moving some old files to a dedicated directory for now

--- DrawPlots/make_histos.py
+++ DrawPlots/make_histos.py
@@ -14,7 +14,7 @@ def main():
     parser.add_argument('-p', '--pdf', action='store_true', help='Save a PDF of each plot. Default is not to save a PDF.')
     parser.add_argument('-w', '--web', action='store_true', help='Post each plot to the user\'s AFS space.')
     parser.add_argument('-s', '--sample', action='append', help='Run on a single sample.  Default is to run on all samples listed in the configuration file.')
-    parser.add_argument('-l', '--lepton_category', action='append', help='Run on a single lepton category.  Default is to run on all lepton categories listed in the configuration file.')    
+    parser.add_argument('-l', '--lepton_category', action='append', help='Run on a single lepton category.  Default is to run on all lepton categories listed in the configuration file.')
     parser.add_argument('-n', '--no_weights', action='store_true', help='Don\'t apply any normalization or weights.')
     parser.add_argument('-f', '--file', help='Run on a single file.  (Must also specify which sample it is with --sample.)')
     parser.add_argument('--label', help='Override the label set in the configuration file with LABEL')
@@ -34,15 +34,15 @@ def main():
     if args.lepton_category:
         lepton_categories = args.lepton_category
 
-    plot_helper.make_sure_directories_exist(lepton_categories)
+    plot_helper.make_sure_directories_exist([os.path.join('histos', category) for category in lepton_categories])
     if args.web:
-        www_plot_directories = ['plots/'+config['label']+'/'+lepton_category for lepton_category in lepton_categories]
+        www_plot_directories = [os.path.join('plots', config['label'], lepton_category) for lepton_category in lepton_categories]
         plot_helper.setup_web_posting(www_plot_directories, 4, args.config_file_name)
 
     if args.batch:
         submit_batch_jobs(config, samples, lepton_categories)
     else:
-        make_plots(args, config, samples, lepton_categories)
+        make_histos(args, config, samples, lepton_categories)
 
     if args.web:
         if args.batch:
@@ -50,7 +50,7 @@ def main():
         else:
             print '\nFinished processing.  Plots will be posted to: http://www.crc.nd.edu/~%s/plots/%s/' % (os.environ['USER'], config['label'])
 
-def make_plots(args, config, samples, lepton_categories):
+def make_histos(args, config, samples, lepton_categories):
     for sample in samples:
         sample_info = plot_helper.SampleInformation(sample)
 
@@ -60,7 +60,7 @@ def make_plots(args, config, samples, lepton_categories):
                     continue
 
             for jet_tag_category in config['jet tag categories']:
-                output_file_name = '%s/%s_%s_%s_%s.root' % (lepton_category, lepton_category, jet_tag_category, sample, config['label'])
+                output_file_name = 'histos/%s/%s_%s_%s_%s.root' % (lepton_category, lepton_category, jet_tag_category, sample, config['label'])
                 output_file = ROOT.TFile(output_file_name, 'RECREATE')
 
                 systematics_info = plot_helper.SystematicsInformation(config['baseline systematics'])
@@ -101,18 +101,17 @@ def make_plots(args, config, samples, lepton_categories):
                 output_file.Close() #end jet tag category
 
 def submit_batch_jobs(config, samples, lepton_categories):
-    if not os.path.exists('batch_logs'):
-        os.makedirs('batch_logs')
+    plot_helper.make_sure_directories_exist(['batch_logs'])
 
     argument_string = ''
     for argument in sys.argv[1:]:
         if argument != '-b' and argument != '-batch':
             argument_string += argument + ' '
 
-    condor_header = 'universe = vanilla \nexecutable = make_plots.py \nnotification = Never \ngetenv = True \n+IsExpressJob = True'
+    condor_header = 'universe = vanilla \nexecutable = make_histos.py \nnotification = Never \ngetenv = True \n+IsExpressJob = True'
     for sample in samples:
         for lepton_category in lepton_categories:
-            condor_submit_file = open('make_plots_batch.submit', 'w')
+            condor_submit_file = open('make_histos_batch.submit', 'w')
             condor_submit_file.write(condor_header)
             condor_submit_file.write('\narguments = -s %s -l %s %s' % (sample, lepton_category, argument_string))
             condor_submit_file.write('\nlog = batch_logs/%s_%s_%s.log' % (config['label'], sample, lepton_category))
@@ -121,7 +120,7 @@ def submit_batch_jobs(config, samples, lepton_categories):
             condor_submit_file.write('\nqueue 1')
             condor_submit_file.close()
 
-            os.popen('condor_submit make_plots_batch.submit')
+            os.popen('condor_submit make_histos_batch.submit')
             print '\nSubmitting batch jobs for sample %s, lepton category %s... ' % (sample, lepton_category)
 
 if __name__ == '__main__':
