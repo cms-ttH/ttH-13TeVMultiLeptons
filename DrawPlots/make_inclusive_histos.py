06f63e1f49fc7f8c6a9bbd333f880cd400f989f6
annawoodard <awoodard@nd.edu>
2014-02-21 15:55:37 -0500

    Fix for nested directories

--- DrawPlots/make_inclusive_histos.py
+++ DrawPlots/make_inclusive_histos.py
@@ -10,14 +10,15 @@ parser = ArgumentParser(description=('Combine lepton categories to make an inclu
 parser.add_argument('directory', help='Histo directory to make inclusive category for.')
 args = parser.parse_args()
 
-lepton_categories = [x for x in glob.glob('%s/*' % args.directory) if 'inclusive' not in x]
+lepton_categories = [x for x in glob.glob(os.path.join(args.directory, '*')) if 'inclusive' not in x]
 file_dict = dict((lep_cat, sorted(glob.glob('%s/*.root' % lep_cat))) for lep_cat in lepton_categories)
 
 if not os.path.exists(os.path.join(args.directory, 'inclusive')):
     os.makedirs(os.path.join(args.directory, 'inclusive'))
 
 for files in zip(*file_dict.values()):
-    base, category, file = files[0].split('/')
+    head, file = os.path.split(files[0])
+    base, category = os.path.split(head)
     output = os.path.join(base, 'inclusive', file.replace(category, 'inclusive'))
 
     command = 'hadd -f %s ' % output + ' '.join(files)
