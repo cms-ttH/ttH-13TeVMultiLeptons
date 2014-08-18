ac52ac97421ff7863e0611f2cb7f165e4993599b
Anna Elizabeth Woodard <awoodard@nd.edu>
2014-08-18 17:04:41 -0400

    Adding missing header file.

---
+++ TemplateMakers/interface/NOVa/LepCuts.h
@@ -0,0 +1,148 @@
+#ifndef _LepCuts_h
+#define _LepCuts_h
+
+#include "ttHMultileptonAnalysis/TemplateMakers/interface/KinematicVariable.h"
+#include "ttHMultileptonAnalysis/TemplateMakers/interface/BranchInfo.h" 
+#include <typeinfo>
+
+template <class collectionType>
+class LepCuts: public KinematicVariable<int> {
+
+public:
+  //Store branch values so they are accessible to other classes
+  vector<BranchInfo<int>> myVars;
+
+  collectionType **selCollection;
+  string branch_name;
+  unsigned int maxLeptons;
+
+  TString branchName;
+
+  LepCuts(collectionType **_selCollection,
+          string _branch_name, int _maxLeptons);
+
+  void evaluate ();
+
+  int computeLepCut(double _pt, int _isElectron, int _isMuon,
+                    double _jetBTagCSV, double _chargedHadronIsoDR04,
+                    double _IP, double _SIP, double _jetPtRatio);
+
+};
+
+template <class collectionType>
+LepCuts<collectionType>::LepCuts(collectionType **_selCollection,
+                                 string _branch_name, int _maxLeptons):
+  selCollection(_selCollection),
+  branch_name(_branch_name), maxLeptons(_maxLeptons)
+{
+  this->resetVal = KinematicVariableConstants::INT_INIT;
+
+  for (unsigned int i=0; i<maxLeptons; i++) {
+    branchName = Form("%s_%d_lepCut", branch_name.c_str(), i+1);
+    branches[branchName] = BranchInfo<int>(branchName);
+    branches[branchName].branchVal = this->resetVal;
+  }
+
+}
+
+template <class collectionType>
+void LepCuts<collectionType>::evaluate () {
+
+  if (this->evaluatedThisEvent) return;
+  evaluatedThisEvent = true;
+
+  //--------
+
+  for (unsigned int iObj = 0; iObj < (*selCollection)->size(); iObj++) {
+    if (iObj < maxLeptons) {
+      branchName = Form("%s_%d_lepCut", branch_name.c_str(), iObj+1);
+
+      branches[branchName].branchVal = computeLepCut(ptr((*selCollection)->at(iObj))->pt,
+                                                     ptr((*selCollection)->at(iObj))->isElectron,
+                                                     ptr((*selCollection)->at(iObj))->isMuon,
+                                                     ptr((*selCollection)->at(iObj))->jetBTagCSV,
+                                                     ptr((*selCollection)->at(iObj))->chargedHadronIsoDR04,
+                                                     ptr((*selCollection)->at(iObj))->IP,
+                                                     ptr((*selCollection)->at(iObj))->SIP,
+                                                     ptr((*selCollection)->at(iObj))->jetPtRatio);
+
+//       if (ptr((*selCollection)->at(iObj))->isElectron) {
+
+//         if ( ptr((*selCollection)->at(iObj))->jetBTagCSV > 0.679 ) {
+//           branches[branchName].branchVal = -1; }
+//         else if ( ptr((*selCollection)->at(iObj))->chargedHadronIsoDR04 < 0 || // < -1 does nothing; < 0 eliminates some
+//                   ptr((*selCollection)->at(iObj))->chargedHadronIsoDR04 / ptr((*selCollection)->at(iObj))->pt > 0.15 ) {
+//           branches[branchName].branchVal = 0; }
+//         else if ( abs(ptr((*selCollection)->at(iObj))->IP) > 0.015 ||
+//                   ptr((*selCollection)->at(iObj))->chargedHadronIsoDR04 / ptr((*selCollection)->at(iObj))->pt > 0.05 ||
+//                   ptr((*selCollection)->at(iObj))->jetPtRatio < 0.6 ) {
+//           branches[branchName].branchVal = 1; }
+//         else branches[branchName].branchVal = 2;
+        
+//       }
+      
+//       else if (ptr((*selCollection)->at(iObj))->isMuon) {
+
+//         if ( ptr((*selCollection)->at(iObj))->jetBTagCSV > 0.679 ) {
+//           branches[branchName].branchVal = -1; }
+//         else if ( abs(ptr((*selCollection)->at(iObj))->SIP) > 4 || 
+//                  ptr((*selCollection)->at(iObj))->chargedHadronIsoDR04 / ptr((*selCollection)->at(iObj))->pt > 0.20 ) {
+//           branches[branchName].branchVal = 0; }
+//         else if ( ptr((*selCollection)->at(iObj))->chargedHadronIsoDR04 / ptr((*selCollection)->at(iObj))->pt > 0.10 ||
+//                   ptr((*selCollection)->at(iObj))->jetPtRatio < 0.6 ) {
+//           branches[branchName].branchVal = 1; }
+//         else branches[branchName].branchVal = 2;
+        
+//       }
+
+    }//End if ( iObj < maxLeptons )
+  }//End for iObj
+
+  //Clean out values from last event
+  myVars.clear();
+
+  for (typename map<TString, BranchInfo<int>>::iterator iBranch = branches.begin();
+       iBranch != branches.end(); iBranch++ ) {
+    myVars.push_back(iBranch->second);
+  }
+}
+
+template <class collectionType>
+int LepCuts<collectionType>::computeLepCut(double pt, int isElectron, int isMuon,
+                  double jetBTagCSV, double chargedHadronIsoDR04,
+                  double IP, double SIP, double jetPtRatio) {
+
+  int lepCut = -2;
+  
+  if (isElectron) {
+    
+    if ( jetBTagCSV > 0.679 ) {
+      lepCut = -1; }
+    else if ( chargedHadronIsoDR04 < 0 || // < -1 does nothing; < 0 eliminates some
+              chargedHadronIsoDR04 / pt > 0.15 ) {
+      lepCut = 0; }
+    else if ( abs(IP) > 0.015 ||
+              chargedHadronIsoDR04 / pt > 0.05 ||
+              jetPtRatio < 0.6 ) {
+      lepCut = 1; }
+    else lepCut = 2;
+    
+  }
+  
+  else if (isMuon) {
+    
+    if ( jetBTagCSV > 0.679 ) {
+      lepCut = -1; }
+    else if ( abs(SIP) > 4 || 
+              chargedHadronIsoDR04 / pt > 0.20 ) {
+      lepCut = 0; }
+    else if ( chargedHadronIsoDR04 / pt > 0.10 ||
+              jetPtRatio < 0.6 ) {
+      lepCut = 1; }
+    else lepCut = 2;
+  }
+
+  return lepCut;
+}
+
+#endif
