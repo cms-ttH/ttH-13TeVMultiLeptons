748c4ab149bc39f3a5349bfa2fc0c20f2bfa3149
Charlie Mueller <charles.mueller@cern.ch>
2014-11-19 15:29:22 +0100

    more updates for eleMVAid

--- TemplateMakers/src/MultileptonAna.cc
+++ TemplateMakers/src/MultileptonAna.cc
@@ -361,53 +361,6 @@ bool MultileptonAna::isGoodMuon(const pat::Muon& iMuon, const float iMinPt, cons
   int tightNumberOfValidPixelHits = muonparams.getParameter<int> ("tightNumberOfValidPixelHits");
   int tightTrackerLayersWithMeasurement = muonparams.getParameter<int> ("tightTrackerLayersWithMeasurement");
   
-  ///////
-  //
-  // ele mva id hack
-  //
-  //////
-  EGammaMvaEleEstimatorFWLite* mvaID_ = new EGammaMvaEleEstimatorFWLite();
-  bool useBinnedVersion_ = true;
-  string method_ = "BDT";
-  EGammaMvaEleEstimatorFWLite::MVAType type_ = EGammaMvaEleEstimatorFWLite::kNonTrig;
-  std::vector<std::string> mvaWeightFiles_;
-  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat1.weights.xml");
-  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat2.weights.xml");
-  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat3.weights.xml");
-  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat4.weights.xml");
-  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat5.weights.xml");
-  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat6.weights.xml");
-  mvaID_->initialize(method_, type_, useBinnedVersion_, mvaWeightFiles_);
-  bool useFull5x5 = true;
-  bool mvaDebug = false;
-  double eleMvaNonTrig = mvaID_->mvaValue(iMuon,vertex,rho,useFull5x5,mvaDebug);
-  bool passesMVA = false;
-  if ( iMuon.pt() < 10 ){
-    if ( abs(iMuon.eta()) > 0. && abs(iMuon.eta()) < 0.8){
-      passesMVA = ( eleMvaNonTrig > 0.47 );
-    }
-    else if ( abs(iMuon.eta()) >= 0.8 && abs(iMuon.eta()) < 1.479){
-      passesMVA = ( eleMvaNonTrig > 0.004 );
-    }
-    else if ( abs(iMuon.eta()) >= 1.479 && abs(iMuon.eta()) <= 2.5){
-      passesMVA = ( eleMvaNonTrig > 0.295 );
-    }
-
-  }
-  else if ( iMuon.pt() >= 10 ) {
-    if ( abs(iMuon.eta()) > 0. && abs(iMuon.eta()) < 0.8){
-      passesMVA = ( eleMvaNonTrig > 0.5 );
-    }
-    else if ( abs(iMuon.eta()) >= 0.8 && abs(iMuon.eta()) < 1.479){
-      passesMVA = ( eleMvaNonTrig > 0.12 );
-    }
-    else if ( abs(iMuon.eta()) >= 1.479 && abs(iMuon.eta()) <= 2.5){
-      passesMVA = ( eleMvaNonTrig > 0.60 );
-    }
-    
-  }
-
-
   switch(iMuonID){
   case muonID::muonSide:
   case muonID::muonSideLooseMVA:
@@ -497,6 +450,53 @@ bool MultileptonAna::isGoodElectron(const pat::Electron& iElectron, const float 
   double looseDxy = electronparams.getParameter<double> ("looseDxy");
   double dz = electronparams.getParameter<double> ("dZ");
   
+  ///////
+  //
+  // ele mva id hack
+  //
+  //////
+  EGammaMvaEleEstimatorFWLite* mvaID_ = new EGammaMvaEleEstimatorFWLite();
+  bool useBinnedVersion_ = true;
+  string method_ = "BDT";
+  EGammaMvaEleEstimatorFWLite::MVAType type_ = EGammaMvaEleEstimatorFWLite::kNonTrig;
+  std::vector<std::string> mvaWeightFiles_;
+  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat1.weights.xml");
+  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat2.weights.xml");
+  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat3.weights.xml");
+  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat4.weights.xml");
+  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat5.weights.xml");
+  mvaWeightFiles_.push_back("EgammaAnalysis/ElectronTools/data/Electrons_BDTG_NonTrigV0_Cat6.weights.xml");
+  mvaID_->initialize(method_, type_, useBinnedVersion_, mvaWeightFiles_);
+  bool useFull5x5 = true;
+  bool mvaDebug = false;
+  double eleMvaNonTrig = mvaID_->mvaValue(iElectron,vertex,rho,useFull5x5,mvaDebug);
+  bool passesMVA = false;
+  if ( iElectron.pt() < 10 ){
+    if ( abs(iElectron.eta()) > 0. && abs(iElectron.eta()) < 0.8){
+      passesMVA = ( eleMvaNonTrig > 0.47 );
+    }
+    else if ( abs(iElectron.eta()) >= 0.8 && abs(iElectron.eta()) < 1.479){
+      passesMVA = ( eleMvaNonTrig > 0.004 );
+    }
+    else if ( abs(iElectron.eta()) >= 1.479 && abs(iElectron.eta()) <= 2.5){
+      passesMVA = ( eleMvaNonTrig > 0.295 );
+    }
+
+  }
+  else if ( iElectron.pt() >= 10 ) {
+    if ( abs(iElectron.eta()) > 0. && abs(iElectron.eta()) < 0.8){
+      passesMVA = ( eleMvaNonTrig > 0.5 );
+    }
+    else if ( abs(iElectron.eta()) >= 0.8 && abs(iElectron.eta()) < 1.479){
+      passesMVA = ( eleMvaNonTrig > 0.12 );
+    }
+    else if ( abs(iElectron.eta()) >= 1.479 && abs(iElectron.eta()) <= 2.5){
+      passesMVA = ( eleMvaNonTrig > 0.60 );
+    }
+    
+  }
+
+
 
   // Be skeptical about this electron making it through
   bool passesKinematics	= false;
@@ -554,7 +554,7 @@ bool MultileptonAna::isGoodElectron(const pat::Electron& iElectron, const float 
       passGsfTrackID = ( (fabs(iElectron.gsfTrack()->dxy(vertex.position())) < 0.05) && (fabs(iElectron.gsfTrack()->dz(vertex.position())) < 0.2) && iElectron.gsfTrack()->trackerExpectedHitsInner().numberOfHits() <= 1  );
     }
     
-    passesID         = ( passGsfTrackID && passMVAId53x && no_exp_inner_trkr_hits && notConv && myTrigPresel );    
+    passesID         = ( passGsfTrackID && passMVAId53x && no_exp_inner_trkr_hits && notConv && myTrigPresel && passesMVA );    
     break;
   }
   
