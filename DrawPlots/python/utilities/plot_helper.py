029bdfa6f7ee1c2b72a297640198ed46232ff711
geoff-smith <geoff127@gmail.com>
2015-02-04 17:30:43 +0100

    more hist+plot-making updates

--- DrawPlots/python/utilities/plot_helper.py
+++ DrawPlots/python/utilities/plot_helper.py
@@ -9,8 +9,9 @@ import time
 import itertools
 import numpy
 import importlib
-DefaultOrderedDict = importlib.import_module('ttH-13TeVMultiLeptons.DrawPlots.utilities.ordereddict', None)
-#from ttHMultileptonAnalysis.DrawPlots.utilities.ordereddict import DefaultOrderedDict
+
+DefaultOrderedDictmod = importlib.import_module('ttH-13TeVMultiLeptons.DrawPlots.utilities.ordereddict', None)
+DefaultOrderedDict = getattr(DefaultOrderedDictmod, 'DefaultOrderedDict')
 
 def append_integral_histo(config):
     if not config['distributions'].has_key('integral_histo'):
@@ -67,7 +68,7 @@ def update_indexes(directory):
         files_snippet = '\n'.join([snippet.format(location=location, name=name, mod_time=mod_time) for (location, name, mod_time) in zip(non_image_item_locations, non_image_item_names, non_image_mod_times)])
         snippet = '<div class="pic photo-link smoothbox" id="{png}"><a href="{pdf}" rel="gallery"><img src="{png}" class="pic"/></a></div>'
         image_snippet = '\n'.join([snippet.format(pdf=pdf, png=png) for (pdf, png) in zip(pdf_images, png_images)])
-        with open(os.path.join(os.environ['CMSSW_BASE'], 'src/ttHMultileptonAnalysis/DrawPlots/python/utilities/index.html'), 'r') as f:
+        with open(os.path.join(os.environ['CMSSW_BASE'], 'src/ttH-13TeVMultiLeptons/DrawPlots/python/utilities/index.html'), 'r') as f:
             index = f.read()
         with open(os.path.join(root, 'index.html'), 'w') as f:
             segment = index[index.find("<body>"):] #Have to do this because all of the javascript looks like python formatting statements
@@ -80,7 +81,7 @@ def setup_www_directory(directory, depth=1, *extra_files_to_post):
     head = directory
     for level in range(depth):
         if not os.path.exists(os.path.join(head, '/index.html')):
-            file_util.copy_file(os.path.join(os.environ['CMSSW_BASE'], 'src/ttHMultileptonAnalysis/DrawPlots/python/utilities/index.html'), head)
+            file_util.copy_file(os.path.join(os.environ['CMSSW_BASE'], 'src/ttH-13TeVMultiLeptons/DrawPlots/python/utilities/index.html'), head)
         head, tail = os.path.split(head)
 
     for file in extra_files_to_post:
@@ -934,12 +935,19 @@ class SampleInformation:
                        'x_section_error': 0.0,
                        'num_generated': 996773},
 
-            'ttH125': {'sample_type': 'MC',
+            #'ttH125': {'sample_type': 'MC',
+#                       'is_signal': True,
+#                       'x_section': 0.1302,
+#                       'x_section_error': 0.0,
+#                       'x_section_error_ttV': 0.095,
+#                       'num_generated': 992997},
+            
+	    'ttH125': {'sample_type': 'MC',
                        'is_signal': True,
-                       'x_section': 0.1302,
+                       'x_section': 0.8696,
                        'x_section_error': 0.0,
                        'x_section_error_ttV': 0.095,
-                       'num_generated': 992997},
+                       'num_generated': 200},
 
             'ttH125_NP': {'sample_type': 'MC',
                           'is_signal': True,
