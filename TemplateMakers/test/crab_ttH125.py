83e03c9e3ededde21094f64b4f7a48c94e297f62
Charlie Mueller <charles.mueller@cern.ch>
2016-02-01 11:25:51 +0100

    76x samples to crab cfg

--- TemplateMakers/test/crab_ttH125.py
+++ TemplateMakers/test/crab_ttH125.py
@@ -1,18 +1,19 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'ttH125'
-config.General.workArea = 'crab3_test'
+config.General.requestName = 'ttH125_aMC_extn'
+config.General.workArea = 'crab3_qgid'
 config.General.transferLogs=True
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'osTwoLep_cfg.py'
 
-config.Data.inputDataset = '/ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ext1-v3/MINIAODSIM'
+#config.Data.inputDataset = '/ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM'
+config.Data.inputDataset = '/ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext1-v1/MINIAODSIM'
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 50000
 #config.Data.totalUnits = 1
-config.Data.outLFNDirBase = '/store/user/gesmith/crab3dir/v4p2p7'
+config.Data.outLFNDirBase = '/store/user/muell149/ttH-leptons_Skims/qgid'
 config.Data.publication = False
 
 config.Site.storageSite = 'T2_CH_CERN'
