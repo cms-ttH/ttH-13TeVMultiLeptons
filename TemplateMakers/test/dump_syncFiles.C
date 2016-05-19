99c993d0a45b60eb0b7861ef23e70030b9fda793
Charlie Mueller <charles.mueller@cern.ch>
2016-05-19 13:08:58 +0200

    speeding up sync code

--- TemplateMakers/test/dump_syncFiles.C
+++ TemplateMakers/test/dump_syncFiles.C
@@ -34,7 +34,7 @@ void dump_muons(std::ofstream& output, vector<ttH::Muon> *preselected_muons, int
 	 <<mu.obj.E()<< " "
 	 <<mu.pdgID<< " "
 	 <<mu.charge<< " "
-	 <<mu.lepMVA<< " "
+	 <<mu.jet_nCharged_tracks<< " "
 	 <<mu.miniIso<< " "
 	 <<mu.miniAbsIsoCharged<< " "
 	 <<mu.miniAbsIsoNeutral<< " "
@@ -47,22 +47,43 @@ void dump_muons(std::ofstream& output, vector<ttH::Muon> *preselected_muons, int
 	 <<mu.segCompatibility<<endl;
 }
 
+void dump_electrons(std::ofstream& output, vector<ttH::Electron> *preselected_electrons, int event)
+{
+  if (preselected_electrons->size() == 0) return;
+
+  ttH::Electron ele = (*preselected_electrons)[0];
+  output << setiosflags(ios::fixed) << setprecision(5);
+  output << event << " "
+	 <<ele.obj.Pt()<< " "
+	 <<ele.obj.Eta()<< " "
+	 <<ele.obj.Phi()<< " "
+	 <<ele.obj.E()<< " "
+	 <<ele.pdgID<< " "
+	 <<ele.charge<< " "
+	 <<ele.jet_nCharged_tracks<< " "
+	 <<ele.miniIso<< " "
+	 <<ele.miniAbsIsoCharged<< " "
+	 <<ele.miniAbsIsoNeutral<< " "
+	 <<ele.jetPtRel<< " "
+	 <<ele.csv<< " "
+	 <<ele.jetPtRatio<< " "
+	 <<ele.sip3D<< " "
+	 <<ele.dxy<< " "
+	 <<ele.dz<< " "
+	 <<ele.mvaID<<endl;
+}
+
 void run_it(TChain* chain)
 {
-
-  //  std::ofstream ele_output; ele_output.open("electron_dump.txt");
+  
+  std::ofstream ele_output; ele_output.open("electron_dump.txt");
   std::ofstream mu_output; mu_output.open("muon_dump.txt");
   
   int chainentries = chain->GetEntries();   
   cout << "# events in tree: "<< chainentries << endl;  
   
   double mcwgt_intree = -999.;
-  double wgt_intree = -999.;
-  double wallTimePerEvent_intree = -99.;  
   int eventnum_intree = -999;
-  int higgs_decay_intree = -9999;
-  int lumiBlock_intree = -999;
-  int runNumber_intree = -999;
   
   vector<ttH::Lepton> *preselected_leptons_intree=0;
   vector<ttH::Electron> *raw_electrons_intree=0;               
@@ -71,36 +92,28 @@ void run_it(TChain* chain)
   vector<ttH::Muon> *preselected_muons_intree=0;
   vector<ttH::Jet> *preselected_jets_intree=0;
   vector<ttH::MET> *met_intree=0;
-  vector<ttH::GenParticle> *pruned_genParticles_intree=0;
-  vector<ttH::Lepton> *tight_leptons_intree=0;
-  vector<ttH::Electron> *tight_electrons_intree=0;
-  vector<ttH::Muon> *tight_muons_intree=0;
-  vector<ttH::Lepton> *tight_leptons_sortedByMiniIso_intree=0;  
-  
+
+  //only enable some branches
+  chain->SetBranchStatus("*",0);
+  chain->SetBranchStatus("mcwgt",1);
+  chain->SetBranchStatus("eventnum",1);
+  chain->SetBranchStatus("preselected_electrons.*",1);
+  chain->SetBranchStatus("preselected_muons.*",1);
+  chain->SetBranchStatus("preselected_jets.*",1);
+  chain->SetBranchStatus("met.*",1);
+
   chain->SetBranchAddress("mcwgt", &mcwgt_intree);
-  chain->SetBranchAddress("wgt", &wgt_intree);
-  chain->SetBranchAddress("wallTimePerEvent", &wallTimePerEvent_intree);
   chain->SetBranchAddress("eventnum", &eventnum_intree);
-  chain->SetBranchAddress("lumiBlock", &lumiBlock_intree);
-  chain->SetBranchAddress("runNumber", &runNumber_intree);
-  chain->SetBranchAddress("higgs_decay", &higgs_decay_intree);
-  chain->SetBranchAddress("preselected_leptons", &preselected_leptons_intree);
   chain->SetBranchAddress("preselected_electrons", &preselected_electrons_intree);
   chain->SetBranchAddress("preselected_muons", &preselected_muons_intree);
-  chain->SetBranchAddress("tightMvaBased_leptons", &tight_leptons_intree);
-  chain->SetBranchAddress("tightMvaBased_electrons", &tight_electrons_intree);
-  chain->SetBranchAddress("tightMvaBased_muons", &tight_muons_intree);    
   chain->SetBranchAddress("preselected_jets", &preselected_jets_intree);
   chain->SetBranchAddress("met", &met_intree);
-  chain->SetBranchAddress("pruned_genParticles", &pruned_genParticles_intree);   
-
 
   double starttime = get_wall_time();
-  //  chainentries = 1000000;
   for (int i=0; i<chainentries; i++)
     {
       
-      if (i%1000 == 0)
+      if (i%7000 == 0)
 	{
 	  float fraction = 100.*i/chainentries;
 	  cout << fraction << " % complete" << endl;
@@ -115,6 +128,7 @@ void run_it(TChain* chain)
       //////////////////////////
       
       dump_muons(mu_output, preselected_muons_intree, eventnum_intree);
+      dump_electrons(ele_output, preselected_electrons_intree, eventnum_intree);
       
     }
   
@@ -124,7 +138,7 @@ void run_it(TChain* chain)
   if (chainentries>0) cout << "an average of " << (endtime - starttime) / chainentries << " per event." << endl;
 
   mu_output.close();
-  //  ele_output.close();
+  ele_output.close();
   
 }
 
@@ -132,7 +146,7 @@ void dump_syncFiles(void)
 {
 
   TChain *chain = new TChain("OSTwoLepAna/summaryTree");
-  chain->Add("tree_BDT2_orig.root");
+  chain->Add("tree_sync.root");
   run_it(chain);
 
 }
