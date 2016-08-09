4d6f91a11a4855b7a931d669c2bfeae96cee6df5
Charlie Mueller <charles.mueller@cern.ch>
2016-08-09 11:28:42 +0200

    adding gen-matching to make selection tree

--- TemplateMakers/test/makeSelectionTree.C
+++ TemplateMakers/test/makeSelectionTree.C
@@ -18,7 +18,7 @@
 #include "TMVA/MethodCuts.h"
 #include "selection.h"
 #include "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/loadSamples_80x.h"
-#include "treeTools.h"
+#include "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/treeTools.h"
 
 /////////////////////////////////////////
 ///
@@ -27,35 +27,10 @@
 ///
 /////////////////////////////////////////
 
-int getChild(ttH::GenParticle foster_parent, std::vector<ttH::GenParticle> gen_particles, int child)
-{
-  double min_dr = 99.;
-  ttH::GenParticle biological_parent;
-  biological_parent.child0 = 6666;
-  biological_parent.child1 = 6666;
-
-  for (const auto & gen_particle : gen_particles)
-    {
-      if ( gen_particle.pdgID != foster_parent.pdgID ) continue;
-      if ( gen_particle.status != 52 ) continue;
-      TLorentzVector foster_parent_tlv = setTlv(foster_parent);
-      TLorentzVector gen_particle_tlv = setTlv(gen_particle);
-      double dr = foster_parent_tlv.DeltaR( gen_particle_tlv );
-      if (dr < min_dr)
-	{
-	  min_dr = dr;
-	  biological_parent = gen_particle;
-	}
-    }
-  
-  if ( child == 0 ) return biological_parent.child0;
-  else if ( child == 1 ) return biological_parent.child1;
-  else return 7777;
-}
-
-
 class GenParticleHelper
 {
+#include "treeTools.h"
+
 private:
   ttH::GenParticle higgs_intree;
   ttH::GenParticle higgs_child_A_intree;
@@ -79,6 +54,23 @@ private:
 
   TString higgs_final_state_intree;
   TString ttbar_final_state_intree;
+
+  //reco-gen matching vars
+  vector<ttH::Jet> *matched_reco_jets_intree=0;
+  vector<ttH::Jet> *unmatched_reco_jets_intree=0;
+  vector<ttH::GenParticle> *matched_gen_jets_intree=0;
+  vector<ttH::GenParticle> *unmatched_gen_jets_intree=0;
+
+  ttH::Lepton *lep_from_higgs_intree=0;
+  ttH::Lepton *lep_from_leptop_intree=0;
+  ttH::Jet *b_from_leptop_intree=0;
+  ttH::Jet *b_from_hadtop_intree=0;
+  ttH::Jet *q1_from_hadtop_intree=0;
+  ttH::Jet *q2_from_hadtop_intree=0;
+  ttH::Jet *q1_from_higgs_intree=0;
+  ttH::Jet *q2_from_higgs_intree=0;
+
+
 
 public:
   GenParticleHelper(){};//default constructor
@@ -121,6 +113,20 @@ public:
     antitop_w_intree = empty_gen_particle;
     antitop_w_child1_intree = empty_gen_particle;
     antitop_w_child2_intree = empty_gen_particle;
+
+    matched_reco_jets_intree->clear();
+    unmatched_reco_jets_intree->clear();
+    matched_gen_jets_intree->clear();
+    unmatched_gen_jets_intree->clear();
+    lep_from_higgs_intree->clear();
+    lep_from_leptop_intree->clear();
+    b_from_leptop_intree->clear();
+    b_from_hadtop_intree->clear();
+    q1_from_hadtop_intree->clear();
+    q2_from_hadtop_intree->clear();
+    q1_from_higgs_intree->clear();
+    q2_from_higgs_intree->clear();
+
   }
 
   void initializeTree(TTree *input_tree)
@@ -148,6 +154,19 @@ public:
     input_tree->Branch("higgs_final_state", &higgs_final_state_intree);
     input_tree->Branch("ttbar_final_state", &ttbar_final_state_intree);
     
+    input_tree->Branch("matched_reco_jets", &matched_reco_jets_intree);
+    input_tree->Branch("unmatched_reco_jets", &unmatched_reco_jets_intree);
+    input_tree->Branch("matched_gen_jets", &matched_gen_jets_intree);
+    input_tree->Branch("unmatched_gen_jets", &unmatched_gen_jets_intree);
+    input_tree->Branch("lep_from_higgs_reco_truth", &lep_from_higgs_intree);
+    input_tree->Branch("lep_from_leptop_reco_truth", &lep_from_leptop_intree);
+    input_tree->Branch("b_from_leptop_reco_truth", &b_from_leptop_intree);
+    input_tree->Branch("b_from_hadtop_reco_truth", &b_from_hadtop_intree);
+    input_tree->Branch("q1_from_hadtop_reco_truth", &q1_from_hadtop_intree);
+    input_tree->Branch("q2_from_hadtop_reco_truth", &q2_from_hadtop_intree);
+    input_tree->Branch("q1_from_higgs_reco_truth", &q1_from_higgs_intree);
+    input_tree->Branch("q2_from_higgs_reco_truth", &q2_from_higgs_intree);
+
   }
 
   void mapGenInfo(vector<ttH::GenParticle> gen_particles)
@@ -188,10 +207,6 @@ public:
 	    
 	    if ( top_w_intree.child1 > gen_particles.size() ) top_w_intree.child1 = top_w_intree.child0 + 1;
 
-	    // cout << "top: " << endl;
-	    // cout << top_w_intree.child0 << endl;
-	    // cout << top_w_intree.child1 << endl;
-
 	    top_w_child1_intree = gen_particles[top_w_intree.child0];
 	    top_w_child2_intree = gen_particles[top_w_intree.child1];
 	    
@@ -211,10 +226,6 @@ public:
 	      }
 
 	    if ( antitop_w_intree.child1 > gen_particles.size() ) antitop_w_intree.child1 = antitop_w_intree.child0 + 1;
-
-	    // cout << "antitop: " << endl;
-	    // cout << antitop_w_intree.child0 << endl;
-	    // cout << antitop_w_intree.child1 << endl;
 
 	    antitop_w_child1_intree = gen_particles[antitop_w_intree.child0];
 	    antitop_w_child2_intree = gen_particles[antitop_w_intree.child1];
@@ -252,6 +263,114 @@ public:
 
   }
 
+  void matchReco2Gen(vector<ttH::Lepton> reco_leps, vector<ttH::Jet> reco_jets, vector<ttH::GenParticle> gen_particles)
+  {
+    //match leptons first
+    for (const auto & lep : reco_leps)
+      {
+	if (abs(lep.genGrandMotherPdgID) == 6) *lep_from_leptop_intree = lep;
+	else if (lep.genGrandMotherPdgID == 25) *lep_from_higgs_intree = lep;
+      }
+    //match jets
+    vector<ttH::GenParticle> hardScatter_genParticles;
+    if ( !isLepton( &higgs_grandChild_A1_intree ) ) hardScatter_genParticles.push_back( higgs_grandChild_A1_intree );
+    if ( !isLepton( &higgs_grandChild_A2_intree ) ) hardScatter_genParticles.push_back( higgs_grandChild_A2_intree );
+    if ( !isLepton( &higgs_grandChild_B1_intree ) ) hardScatter_genParticles.push_back( higgs_grandChild_B1_intree );
+    if ( !isLepton( &higgs_grandChild_B2_intree ) ) hardScatter_genParticles.push_back( higgs_grandChild_B2_intree );
+    if ( !isLepton( &top_b_intree ) ) hardScatter_genParticles.push_back( top_b_intree );
+    if ( !isLepton( &top_w_child1_intree ) ) hardScatter_genParticles.push_back( top_w_child1_intree );
+    if ( !isLepton( &top_w_child2_intree ) ) hardScatter_genParticles.push_back( top_w_child2_intree );
+    if ( !isLepton( &antitop_b_intree ) ) hardScatter_genParticles.push_back( antitop_b_intree );
+    if ( !isLepton( &antitop_w_child1_intree ) ) hardScatter_genParticles.push_back( antitop_w_child1_intree );
+    if ( !isLepton( &antitop_w_child2_intree ) ) hardScatter_genParticles.push_back( antitop_w_child2_intree );
+    
+    
+    std::vector<ttH::Jet> temp_unmatched_reco_jets;
+    std::vector<ttH::GenParticle> temp_unmatched_gen_jets;
+    
+    for (const auto & jet : reco_jets)
+      {
+	if (jet.genPdgID == 9999)
+	  {
+	    temp_unmatched_reco_jets.push_back(jet);
+	    continue;
+	  }
+	for (const auto & gen_jet : hardScatter_genParticles)
+	  {
+	    double deltaR = getDeltaR(jet, gen_jet);
+	    if ( jet.genPdgID == gen_jet.pdgID && deltaR <= 0.5  )
+	      {
+		matched_reco_jets_intree->push_back(jet);
+		matched_gen_jets_intree->push_back(gen_jet);
+		
+		if ( gen_jet.obj.pt() == top_b_intree.obj.pt() )
+		  {
+		    if ( lep_from_leptop_intree->genGrandMotherPdgID == 6 ) *b_from_leptop_intree = jet;
+		    else *b_from_hadtop_intree = jet;
+		  }
+		else if ( gen_jet.obj.pt() == antitop_b_intree.obj.pt() )
+		  {
+		    if ( lep_from_leptop_intree->genGrandMotherPdgID == -6 ) *b_from_leptop_intree = jet;
+		    else *b_from_hadtop_intree = jet;
+		  }
+		else if ( gen_jet.obj.pt() == higgs_grandChild_A1_intree.obj.pt() || gen_jet.obj.pt()== higgs_grandChild_B1_intree.obj.pt() ) 
+		  {
+		    *q1_from_higgs_intree = jet;
+		  }
+		else if ( gen_jet.obj.pt() == higgs_grandChild_A2_intree.obj.pt() || gen_jet.obj.pt()== higgs_grandChild_B2_intree.obj.pt() ) 
+		  {
+		    *q2_from_higgs_intree = jet;
+		  }
+		else if ( gen_jet.obj.pt() == top_w_child1_intree.obj.pt() || gen_jet.obj.pt() == antitop_w_child1_intree.obj.pt() )
+		  {
+		    *q1_from_hadtop_intree = jet;
+		  }
+		else if ( gen_jet.obj.pt() == top_w_child2_intree.obj.pt() || gen_jet.obj.pt() == antitop_w_child2_intree.obj.pt() )
+		  {
+		    *q2_from_hadtop_intree = jet;
+		  }
+		
+		break;
+	      }
+	  }
+      }
+    
+    *unmatched_gen_jets_intree = vector_difference<ttH::GenParticle>(*matched_gen_jets_intree, hardScatter_genParticles);
+    
+    for (const auto & gen_j : *unmatched_gen_jets_intree)
+      {
+	if ( gen_j.obj.pt() > 25 && abs(gen_j.obj.eta()) < 2.4 ) temp_unmatched_gen_jets.push_back(gen_j);
+      }
+    
+    for (auto & reco_j : temp_unmatched_reco_jets)
+      {
+	double min_dr = 99.;
+	ttH::GenParticle matched_gen_j;
+	for (const auto & gen_j : temp_unmatched_gen_jets)
+	  {
+	    double dr = getDeltaR(reco_j, gen_j);
+	    if (dr < min_dr)
+	      {
+		min_dr = dr;
+		matched_gen_j = gen_j;
+	      }
+	  }
+	if (min_dr <= 0.4 && ( abs(1. - matched_gen_j.obj.pt()/reco_j.obj.pt()) < 0.5 ) )
+	  {
+	    reco_j.genPdgID = matched_gen_j.pdgID; 
+	    reco_j.genMotherPdgID = gen_particles[matched_gen_j.mother].pdgID; 
+	    reco_j.genGrandMotherPdgID = gen_particles[matched_gen_j.grandmother].pdgID; 
+	    matched_reco_jets_intree->push_back( reco_j );
+	    matched_gen_jets_intree->push_back( matched_gen_j );
+	    
+      	    }
+      }
+    
+    *unmatched_reco_jets_intree = vector_difference<ttH::Jet>(*matched_reco_jets_intree, reco_jets);
+    *unmatched_gen_jets_intree = vector_difference<ttH::GenParticle>(*matched_gen_jets_intree, hardScatter_genParticles);
+    
+  }
+  
   virtual ~GenParticleHelper(){};
 };
 
@@ -325,6 +444,7 @@ void run_it(TChain* chain, TString output_file)
 	{
 	  myGenParticleHelper.clear();
 	  myGenParticleHelper.mapGenInfo(*pruned_genParticles_intree);
+	  myGenParticleHelper.matchReco2Gen(*tight_leptons_intree, *preselected_jets_intree, *pruned_genParticles_intree);
 	  ss2l_tree->Fill();
 	}
       // else
@@ -356,7 +476,7 @@ void makeSelectionTree(TString sample, int start_file=0, int end_file=0)
 
   TString output_dir = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/";
 
-  TString output_file = sample + "_selection_tree.root";
+  TString output_file = sample + "_selection_tree_testtt.root";
   TChain *tth_chain = loadFiles(sample,start_file,end_file);  
   run_it(tth_chain,output_file);
 
