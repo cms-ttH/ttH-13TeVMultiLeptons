2b80a308fda384dde5b39c3b9f26215ff8746ffa
Charlie <charles.mueller@cern.ch>
2016-10-16 18:36:32 -0400

    updates to limit running script

--- TemplateMakers/test/run_limits.sh
+++ TemplateMakers/test/run_limits.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+#usage: ./run_limits.sh fatStackPlots.root
 
-cd /afs/crc.nd.edu/user/c/cmuelle2/CMSSW_7_4_16_patch2_combine/src/
-export SCRAM_ARCH=slc6_amd64_gcc491
-eval `scramv1 runtime -sh`
-./mk_datacard ~/CMSSW_8_0_14/src/ttH-13TeVMultiLeptons/TemplateMakers/test/fatStackPlots.root
-combine -M Asymptotic -m125 -d limits/125/ttH_ttl.txt
+hists_file=$1
+file_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/$hists_file"
+
+if [ $hists_file ] && [ -a $file_path ]
+    then
+    cd /afs/crc.nd.edu/user/c/cmuelle2/CMSSW_7_4_16_patch2_combine/src/
+    export SCRAM_ARCH=slc6_amd64_gcc491
+    eval `scramv1 runtime -sh`
+    ./mk_datacard $file_path
+    combine -M Asymptotic -m125 -d limits/125/ttH_ttl.txt
+else
+    echo "Please specify file containing hists to calculate limits from."
+    echo "Example: ./run_limits.sh fatStackPlots.root"
+fi
