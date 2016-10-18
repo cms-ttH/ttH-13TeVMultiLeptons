a241c3cadc5f8408449c4bbbb03927343003d97d
Charlie <charles.mueller@cern.ch>
2016-10-18 11:15:46 -0400

    minor udpate

--- TemplateMakers/test/makeSigExtractionTrees.C
+++ TemplateMakers/test/makeSigExtractionTrees.C
@@ -10,6 +10,7 @@
 #include "TH1D.h"
 #include "TFile.h"
 #include <cmath>
+#include <math.h>
 #include "TLorentzVector.h"
 #include "DataFormats/Math/interface/deltaR.h"
 #include "ttH-13TeVMultiLeptons/TemplateMakers/src/classes.h"
@@ -45,7 +46,7 @@ void run_it(TChain* chain, TFile *io_file_)
   vector<ttH::Lepton> *preselected_leptons_intree=0;
   vector<ttH::Lepton> *loose_leptons_intree=0;
   vector<ttH::Electron> *raw_electrons_intree=0;               
-  vector<ttH::Electron> *preselected_electrons_intree=0;
+  vector<ttH::Electron> *loose_electrons_intree=0;
   vector<ttH::Muon> *raw_muons_intree=0;
   vector<ttH::Muon> *preselected_muons_intree=0;
   vector<ttH::Jet> *preselected_jets_intree=0;
@@ -71,12 +72,16 @@ void run_it(TChain* chain, TFile *io_file_)
   chain->SetBranchStatus("mcwgt",1);
   chain->SetBranchStatus("preselected_jets.*",1);
   chain->SetBranchStatus("looseMvaBased_leptons.*",1);
+  chain->SetBranchStatus("preselected_leptons.*",1);
+  chain->SetBranchStatus("looseMvaBased_electrons.*",1);
   chain->SetBranchStatus("tightMvaBased_leptons.*",1);
   chain->SetBranchStatus("met.*",1);
   chain->SetBranchStatus("reco_score",1);
   
   chain->SetBranchAddress("preselected_jets", &preselected_jets_intree);
   chain->SetBranchAddress("looseMvaBased_leptons", &loose_leptons_intree);
+  chain->SetBranchAddress("preselected_leptons", &preselected_leptons_intree);
+  chain->SetBranchAddress("looseMvaBased_electrons", &loose_electrons_intree);
   chain->SetBranchAddress("tightMvaBased_leptons", &tight_leptons_intree);
   chain->SetBranchAddress("met", &met_intree);
   
@@ -91,28 +96,38 @@ void run_it(TChain* chain, TFile *io_file_)
   double l2_pt_branch;
   bool bTight_branch;
 
+  double l1_conePt_branch;
+  double csv1_branch;
+  double csv2_branch;
 
   TTree *ttH_vs_ttbar_tree = (TTree*)chain->CloneTree(0);
   ttH_vs_ttbar_tree->SetName("tth_vs_ttbar_tree");
-  ttH_vs_ttbar_tree->Branch("max_lep_eta", &max_lep_eta_branch);
-  ttH_vs_ttbar_tree->Branch("nJets", &njets_branch);
-  ttH_vs_ttbar_tree->Branch("dR_l1_j", &dR_l1_j_branch);
-  ttH_vs_ttbar_tree->Branch("dR_l2_j", &dR_l2_j_branch);
-  ttH_vs_ttbar_tree->Branch("met", &met_branch);  
-  ttH_vs_ttbar_tree->Branch("avg_dR_jets", &avg_dr_jets_branch);
-  ttH_vs_ttbar_tree->Branch("MT_l1_met", &mt_lep1_met_branch);
+  ttH_vs_ttbar_tree->Branch("max_Lep_eta", &max_lep_eta_branch);
+  ttH_vs_ttbar_tree->Branch("numJets_float", &njets_branch);
+  ttH_vs_ttbar_tree->Branch("mindr_lep1_jet", &dR_l1_j_branch);
+  ttH_vs_ttbar_tree->Branch("mindr_lep2_jet", &dR_l2_j_branch);
+  ttH_vs_ttbar_tree->Branch("met_double", &met_branch);  
+  ttH_vs_ttbar_tree->Branch("avg_dr_jet", &avg_dr_jets_branch);
+  ttH_vs_ttbar_tree->Branch("MT_met_lep1", &mt_lep1_met_branch);
   ttH_vs_ttbar_tree->Branch("isBtight", &bTight_branch);
+  ttH_vs_ttbar_tree->Branch("lep1_conePt", &l1_conePt_branch);
+  ttH_vs_ttbar_tree->Branch("lep1_Pt", &l1_pt_branch);
+  ttH_vs_ttbar_tree->Branch("csv1", &csv1_branch);
+  ttH_vs_ttbar_tree->Branch("csv2", &csv2_branch);
+
 
   TTree *ttH_vs_ttV_tree = (TTree*)chain->CloneTree(0);
   ttH_vs_ttV_tree->SetName("tth_vs_ttV_tree");
-  ttH_vs_ttV_tree->Branch("max_lep_eta", &max_lep_eta_branch);
-  ttH_vs_ttV_tree->Branch("nJets", &njets_branch);
-  ttH_vs_ttV_tree->Branch("dR_l1_j", &dR_l1_j_branch);
-  ttH_vs_ttV_tree->Branch("dR_l2_j", &dR_l2_j_branch);
-  ttH_vs_ttV_tree->Branch("MT_l1_met", &mt_lep1_met_branch);
+  ttH_vs_ttV_tree->Branch("max_Lep_eta", &max_lep_eta_branch);
+  ttH_vs_ttV_tree->Branch("numJets_float", &njets_branch);
+  ttH_vs_ttV_tree->Branch("mindr_lep1_jet", &dR_l1_j_branch);
+  ttH_vs_ttV_tree->Branch("mindr_lep2_jet", &dR_l2_j_branch);
+  ttH_vs_ttV_tree->Branch("MT_met_lep1", &mt_lep1_met_branch);
   ttH_vs_ttV_tree->Branch("l1_pt", &l1_pt_branch);
   ttH_vs_ttV_tree->Branch("l2_pt", &l2_pt_branch);
   ttH_vs_ttV_tree->Branch("isBtight", &bTight_branch);
+  ttH_vs_ttV_tree->Branch("csv1", &csv1_branch);
+  ttH_vs_ttV_tree->Branch("csv2", &csv2_branch);
 
   Int_t cachesize = 250000000;   //250 MBytes
   chain->SetCacheSize(cachesize);
@@ -134,6 +149,7 @@ void run_it(TChain* chain, TFile *io_file_)
 
       //      auto lepton_collection_to_use = tight_leptons_intree;
       auto lepton_collection_to_use = loose_leptons_intree;
+      //      auto lepton_collection_to_use = preselected_leptons_intree;
 
       // max eta
       max_lep_eta_branch = -1.;
@@ -164,19 +180,61 @@ void run_it(TChain* chain, TFile *io_file_)
       l1_pt_branch = lep1.obj.pt();
       l2_pt_branch = lep2.obj.pt();
 
+
+      bool skip_event = false;
+      for (const auto & ele : *loose_electrons_intree)
+	{
+	  if ( (lep1.obj.pt() == ele.obj.pt() || lep2.obj.pt() == ele.obj.pt()) && ele.numMissingInnerHits > 0)
+	    {
+	      skip_event = true;
+	      break;
+	    }
+	}
+      if ( skip_event ) continue;
+
+      auto csv_sorted_jets = *preselected_jets_intree;
+      std::sort(csv_sorted_jets.begin(), csv_sorted_jets.end(), [] (ttH::Jet a, ttH::Jet b) { return a.csv > b.csv;});
+      csv1_branch = csv_sorted_jets[0].csv;
+      csv2_branch = max( csv_sorted_jets[1].csv, 0.);
+
       ttH::Jet lep1_closest_jet = getClosestJet(*preselected_jets_intree, lep1);
       ttH::Jet lep2_closest_jet = getClosestJet(*preselected_jets_intree, lep2);
       dR_l1_j_branch = getDeltaR(lep1_closest_jet, lep1);
       dR_l2_j_branch = getDeltaR(lep2_closest_jet, lep2);
 
       // met
-      met_branch = (*met_intree)[0].obj.Pt();
+      met_branch = min((*met_intree)[0].obj.Pt(),400.);
 
       TLorentzVector met_tlv = setTlv( (*met_intree)[0] );
       TLorentzVector lep1_t_tlv = setTlv( lep1 );
-      lep1_t_tlv.SetPxPyPzE(lep1_t_tlv.Px(), lep1_t_tlv.Py(), 0., lep1_t_tlv.Pt());
-      TLorentzVector lep1_met_t_tlv = met_tlv + lep1_t_tlv;
-      mt_lep1_met_branch = lep1_met_t_tlv.M();
+      
+      bool isTight = false;
+      for (const auto & lep: *tight_leptons_intree)
+	{
+	  if (lep1.obj.pt() == lep.obj.pt())
+	    {
+	      isTight = true;
+	      break;
+	    }
+	}
+
+      if ( !isTight )
+	{
+	  //	  lep1_t_tlv.SetPxPyPzE(lep1_t_tlv.Px()*0.85/lep1.jetPtRatio, lep1_t_tlv.Py()*0.85/lep1.jetPtRatio, 0., lep1_t_tlv.Pt()/lep1.jetPtRatio*0.85);
+	  //	  lep1_t_tlv.SetPxPyPzE(lep1_closest_jet.obj.px()*0.85, lep1_closest_jet.obj.py()*0.85, 0., lep1_closest_jet.obj.pt()*0.85);
+	  //	  l1_conePt_branch = lep1_closest_jet.obj.pt()*0.85;
+	  l1_conePt_branch = lep1.obj.pt()/lep1.jetPtRatio*0.85;
+	}
+      else 
+	{
+	  //	  lep1_t_tlv.SetPxPyPzE(lep1_t_tlv.Px(), lep1_t_tlv.Py(), 0., lep1_t_tlv.Pt());
+	  l1_conePt_branch = lep1.obj.pt();
+	}
+
+      //      TLorentzVector lep1_met_t_tlv = met_tlv + lep1_t_tlv;
+      //      mt_lep1_met_branch = lep1_met_t_tlv.M();
+
+      mt_lep1_met_branch = sqrt(2*l1_conePt_branch*met_branch*(1-cos(lep1.obj.phi()-(*met_intree)[0].obj.phi())));
 
       //calculate avg dr jets
       int jet1_counter = 0;
@@ -217,10 +275,20 @@ void run_it(TChain* chain, TFile *io_file_)
 
 void makeSigExtractionTrees(void)
 {
-  //  TString output_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v1p5_bTightLoose/tth_pow_relaxed_2lss_bdtEval_v1p5__test.root";
-  TString output_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/signal_extraction/training/ttbar_mg5MLM_forSigExtractionTraingLoosenedSelection_bdtEval_v1p5__test.root";
-  TFile *io_file = new TFile(output_file, "UPDATE"); // #, 'test' ) // "RECREATE");
-  TChain *chain = new TChain("ss2l_tree");
-  chain->Add(output_file);
-  run_it(chain,io_file);
+  //  TString input_file1 = "selection_trees/ttbar-semiLep-madgraph_moreRelaxedTrainSelection_2lss.root";
+  TString input_file1 = "selection_trees/ttbar-semiLep-madgraph_moreRelaxedTrainSelection_2lss_conePt.root";
+  TString output_file1 = "signal_extraction/training/ttbar-semiLep-madgraph_evenMoreRelaxedTrainSelection_2lss_BDTtraining_lepConePt.root";
+  TFile *io_file1 = new TFile(output_file1, "RECREATE"); // #, 'test' ) // "RECREATE");
+  TChain *chain1 = new TChain("ss2l_tree");
+  chain1->Add(input_file1);
+
+  run_it(chain1,io_file1);
+
+  TString input_file2 = "selection_trees/tth_powheg_old_moreRelaxedTrainSelection_2lss_conePt.root";
+  TString output_file2 = "signal_extraction/training/tth_powheg_old_moreRelaxedTrainSelection_2lss_BDTtraining_lepConePt.root";
+  TFile *io_file2 = new TFile(output_file2, "RECREATE"); // #, 'test' ) // "RECREATE");
+  TChain *chain2 = new TChain("ss2l_tree");
+  chain2->Add(input_file2);
+
+  run_it(chain2,io_file2);
 }
