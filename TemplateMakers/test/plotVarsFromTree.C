8ecb1f3bf4b7f05a04d623fc901826a48b712495
Charlie <charles.mueller@cern.ch>
2016-11-10 15:03:34 -0500

    much-needed improvement to quick tree-branch plotting tool

--- TemplateMakers/test/plotVarsFromTree.C
+++ TemplateMakers/test/plotVarsFromTree.C
@@ -1,142 +1,95 @@
-void plotHist(TH1D* sig_h, TH1D* bkg_h, TString title)
+///////////////////////////
+///
+/// charlie mueller 11/10/2016
+/// usage: root -l plotRocFromTree.C
+///
+///////////////////////////
+
+class PlotObject
 {
-  gStyle->SetOptStat(0);
-  gStyle->SetTitleBorderSize(0);
-  //  gStyle->SetLegendBorderSize(0); 
-  TCanvas* can1 = new TCanvas("can", "can");
-  can1->SetGrid();
+private:
+public:
+  PlotObject(TString file_str_="sfile", TString tree_name_="ss2l_tree", TString legend_str_="leg", TString var_name_="var", TCut cuts_="",int num_bins_=1, double xmin_=-1., double xmax_=1., int color=1)
+  {
+    cuts_ *= "mcwgt";    
+    TFile *file= new TFile(file_str_,"READONLY");
+    TTree *tree = (TTree*)file->Get(tree_name_);
+    hist = new TH1D("hist","",num_bins_,xmin_,xmax_);
+    TString drawString = var_name_ + ">> hist";
+    tree->Draw(drawString, cuts_, "goffnorm");
+    hist->SetLineColor(color);
+    hist->SetLineWidth(2);
+    hist->GetXaxis()->SetTitle(var_name_);
+    hist->GetYaxis()->SetTitle("normalized units");
+    legend_str = legend_str_;
+    
+  }//default constructor
+  TH1D* hist;
+  TString legend_str;
+  ~PlotObject(){}
+};
 
-  sig_h->SetTitle("");
-  // sig_h->GetXaxis()->SetRangeUser(0.,1.4);
-  sig_h->GetXaxis()->SetTitle(title);
-  sig_h->GetYaxis()->SetTitle("normalized units");
 
-  sig_h->SetFillColor(2);  
-  bkg_h->SetFillColor(4);
-  bkg_h->SetFillStyle(3001);
-  sig_h->Draw("hist");
-  bkg_h->Draw("histsame");
-  
-  if (sig_h->GetMaximum() < bkg_h->GetMaximum())
-    {
-      sig_h->SetMaximum(bkg_h->GetMaximum()*1.1);
-    }
-  
-  TLegend *leg = new TLegend(0.6135057,0.7097458,0.8563218,0.8283898,NULL,"brNDC");
-  leg->AddEntry(sig_h,"ttH","F");
-  leg->AddEntry(bkg_h,"ttbar","F");
-  leg->SetFillColor(0);
-  leg->Draw("same");
-  can1->SaveAs("plot.pdf");  
-
-}
-
-void plotRoc(TH1D* sig, TH1D* bkg, TString title)
+void drawPlots(vector<PlotObject> plot_vec)
 {
-  int num_bins = sig->GetNbinsX()+1;
-  double sig_eff[num_bins];
-  double bkg_rej[num_bins];  
-  float full_integral_sig = sig->Integral();
-  float full_integral_bkg = bkg->Integral();
-  float integral_sig = 0.;
-  float integral_bkg = 0.;
-  for (int bin=0; bin < num_bins; bin++)
-    {
-      integral_sig += sig->GetBinContent(bin);
-      integral_bkg += bkg->GetBinContent(bin);
-      
-      sig_eff[bin] = (full_integral_sig - integral_sig) / full_integral_sig;
-      bkg_rej[bin] = integral_bkg / full_integral_bkg;      
-    }
-
-  TGraph* roc_curve = new TGraph(num_bins,sig_eff,bkg_rej);
-  TString hist_title = title + " - 2lss";
-  roc_curve->SetTitle(hist_title);
-  roc_curve->GetXaxis()->SetRangeUser(0.,1.);
-  roc_curve->GetYaxis()->SetRangeUser(0.,1.);
-  roc_curve->GetXaxis()->SetTitle("signal efficiency");
-  roc_curve->GetYaxis()->SetTitle("background rejection");
-  roc_curve->SetLineWidth(2);
-
-  char label1[512];
-  float roc_integral1 = roc_curve->Integral()+0.5;
-  sprintf(label1,"ROC: %.5f",roc_integral1);
-
   gStyle->SetOptStat(0);
   gStyle->SetTitleBorderSize(0);
   gStyle->SetLegendBorderSize(0); 
-  TCanvas* can1 = new TCanvas("roc can", "roc can");
+  TCanvas* can1 = new TCanvas("can", "can");
   can1->SetGrid();
-
-  roc_curve->SetLineColor(2);
-  roc_curve->Draw("AC");
   TLegend *leg = new TLegend(0.1925287,0.3474576,0.5732759,0.4661017,NULL,"brNDC");
   leg->SetTextSize(0.03); 
-  leg->AddEntry(roc_curve,label1,"l");
+  
+  double hist_max = -1.;
+  for (const auto & plotObj : plot_vec)
+    {
+      if (plotObj.hist->GetMaximum() > hist_max)
+  	{
+  	  hist_max = plotObj.hist->GetMaximum();
+  	  plotObj.hist->Draw("hist");
+  	}
+    }
 
+  for (auto & plotObj : plot_vec)
+    {
+      plotObj.hist->Draw("histsame");
+      leg->AddEntry(plotObj.hist, plotObj.legend_str, "l");
+    }
   leg->SetFillColor(0);
   leg->Draw("same");
-  can1->SaveAs("roc.pdf");  
+  can1->SaveAs("plot.pdf");  
 }
 
 void plotVarsFromTree(void)
 {
-
-  TString signal_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v1p5_bTightLoose/ttH_aMCatNLO_bdtEval.root";
-  //  TString background_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v1p5_bTightLoose/ttV_bdtEval.root";
-  TString background_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v1p5_bTightLoose/ttbar_powheg_bdtEval.root";
-
-  // TString signal_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v0p6/ttH-powheg_batch_bdtEval.root";
-  // TString background_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v0p6/ttbar-semiLep-powheg_batch_bdtEval.root";
-
-  // TString signal_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v0_July12_1541/ttH_tree.root";
-  // TString background_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v0_July12_1541/ttbar_tree.root";
-
-  // TString signal_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v1_Aug8/ttH_tree.root";
-  // TString background_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v1_Aug8/ttbar_tree.root";
-
-  // TString signal_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v1p5/ttH_bdtEval.root";
-  // TString background_file = "/afs/cern.ch/user/m/muell149/work/CMSSW_8_0_13/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/bdt_v1p5/ttbar_bdtEval.root";
-
-  TChain *chain1 = new TChain("ss2l_tree");
-  chain1->Add(signal_file);
-  TChain *chain2 = new TChain("ss2l_tree");
-  chain2->Add(background_file);
-
-  //  TCut cut = "mcwgt*!(b_from_hadtop_bdt.csv >= 0.8 && b_from_leptop_bdt.csv >=0.8)";
-  TCut cut = "mcwgt*(1.)";
-  //  TString var = "lepTop_hadTop_dR"; TString title = "dR(lep top, had top)";
-  //  TString var = "hadTop_bdt.M()"; TString title = "hadronic top mass"; int nBins = 20; double xMin = 0; double xMax = 250;
-  //  TString var = "higgs_bdt.M()"; TString title = "higgs mass"; int nBins = 40; double xMin = 0; double xMax = 150;
-  //  TString var = "higgs_bdt.Pt()"; TString title = "higgs pt"; int nBins = 40; double xMin = 0; double xMax = 250;
-  //  TString var = "dR_b_W_lepTop_bdt"; TString title = "dR(l,b) leptop"; int nBins = 10; double xMin = 0; double xMax = 5;
-  //  TString var = "dR_lepTop_hadTop_bdt"; TString title = "dR(Th,Tl) leptop"; int nBins = 10; double xMin = 0; double xMax = 5;
-  TString var = "reco_score"; TString title = "bdt v1.5 bT/L"; int nBins = 25; double xMin = -1.; double xMax = 1.;
-  //  TString var = "lepTop_tlv.M()"; TString title = "leptonic top mass"; int nBins = 30; double xMin = 0; double xMax = 700;
-  //  TString var = "lepTop_tlv.Pt()"; TString title = "leptonic top pt"; int nBins = 20; double xMin = 0; double xMax = 800;
-  //  TString var = "hadTop_bdt.Pt()"; TString title = "hadronic top pt"; int nBins = 20; double xMin = 0; double xMax = 600;
-  //  TString var = "hadTop_tlv.M() + lepTop_tlv.M()"; TString title = "top sys mass"; int nBins = 30; double xMin = 0; double xMax = 1000;
-  //  TString var = "ttbar_tlv.Pt()"; TString title = "top sys pt"; int nBins = 30; double xMin = 0; double xMax = 700;
-  //  TString var = "abs(ttbar_tlv.Eta())"; TString title = "top sys eta"; int nBins = 30; double xMin = 0; double xMax = 5;
-  //  TString var = "abs(lepTop_tlv.Eta())"; TString title = "lep top eta"; int nBins = 30; double xMin = 0; double xMax = 5;
-  //  TString var = "abs(hadTop_tlv.Eta())"; TString title = "had top eta"; int nBins = 30; double xMin = 0; double xMax = 5;
-  //  TString var = "lepTop_T_tlv.M()"; TString title = "lep top transverse mass"; int nBins = 30; double xMin = 0; double xMax = 700;
-  TString draw_sig = var + " >> sig_h";
-  TString draw_bkg = var + " >> bkg_h";
-
-  TH1D* sig_h = new TH1D("sig_h","sig_h",nBins,xMin,xMax);
-  TH1D* bkg_h = new TH1D("bkg_h","bkg_h",nBins,xMin,xMax);
-
-  chain2->Draw(draw_bkg,cut,"goffnorm");
-  chain1->Draw(draw_sig,cut,"goffnorm");
-
-
-
-  plotHist(sig_h, bkg_h, title);
-  plotRoc(sig_h, bkg_h, title);
-  //  plotRoc(bkg_h, sig_h, title);
+  vector<PlotObject> plot_vector;
+  TString variable_name = "reco_score";
+  int num_bins = 70;
+  double xmin = 0;
+  double xmax = 1.04;
   
+  TString file1 = "/afs/crc.nd.edu/user/c/cmuelle2/CMSSW_8_0_14/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/test/tth__recoBdt_2lss.root";
+  
+  // TCut cuts1 = "W_from_hadtop_matching==5 && b_from_hadtop_matching >= 4";
+  // TCut cuts2 = "(W_from_hadtop_matching==4 && b_from_hadtop_matching >= 4) || (W_from_hadtop_matching==5 && b_from_hadtop_matching < 4)";
 
+  TCut cuts1 = "b_from_hadtop_reco_truth.obj.pt() > 0. && q1_from_hadtop_reco_truth.obj.pt() >0. && q2_from_hadtop_reco_truth.obj.pt() >0.";
+  TCut cuts2 = "(b_from_hadtop_reco_truth.obj.pt()*q1_from_hadtop_reco_truth.obj.pt() >0||b_from_hadtop_reco_truth.obj.pt()*q2_from_hadtop_reco_truth.obj.pt()>0 ||q1_from_hadtop_reco_truth.obj.pt()*q2_from_hadtop_reco_truth.obj.pt()>0)  && b_from_hadtop_reco_truth.obj.pt()*q1_from_hadtop_reco_truth.obj.pt()*q2_from_hadtop_reco_truth.obj.pt()==0";
+  TCut cuts3 = !cuts1 && !cuts2;
+
+
+  //colors = 472
+
+  PlotObject plot1(file1, "ss2l_tree", "full hadtop in acceptance", "reco_score", cuts1, num_bins, xmin, xmax, 8);
+  plot_vector.push_back(plot1);
+
+  PlotObject plot2(file1, "ss2l_tree", "2/3 hadtop in acceptance", "reco_score", cuts2, num_bins, xmin, xmax, 41);
+  plot_vector.push_back(plot2);
+
+  PlotObject plot3(file1, "ss2l_tree", "<=1/3 hadtop in acceptance", "reco_score", cuts3, num_bins, xmin, xmax, 4);
+  plot_vector.push_back(plot3);
+
+  drawPlots(plot_vector);
 }
 
 
