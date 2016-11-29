80831ac32542758a5b386b32807c50a639e813dc
Charlie <charles.mueller@cern.ch>
2016-11-29 15:30:34 -0500

    hadd files only on panasas for speed

--- TemplateMakers/test/condor_test/hadd_files.sh
+++ TemplateMakers/test/condor_test/hadd_files.sh
@@ -8,15 +8,16 @@ prefix=$1
 #target_dir="/afs/crc.nd.edu/user/c/cmuelle2/CMSSW_8_0_14/src/ttH-13TeVMultiLeptons/TemplateMakers/test/selection_trees/condor"
 #target_dir="/scratch365/cmuelle2/selection_trees/nov22_ICHEP_trees"
 #target_dir="/scratch365/cmuelle2/extraction_trees/nov22_ICHEP_trees"
-target_dir="/scratch365/cmuelle2/bdt_test"
+#target_dir="/scratch365/cmuelle2/bdt_test"
+target_dir="/scratch365/cmuelle2/extraction_trees/nov22_ICHEP_trees_with_WgtHist"
 
 files="$target_dir/$prefix*.root" 
 
 #postfix="_genFilterTraining_2lss.root"
-#postfix="_2lss_extraction.root"
+postfix="_2lss_extraction.root"
 #postfix="_2lss_selection.root"
-postfix="_2lss_trainingSelection.root"
-output="$prefix$postfix"
+#postfix="_2lss_trainingSelection.root"
+output="$target_dir/../$prefix$postfix"
 
 export SCRAM_ARCH=slc6_amd64_gcc530
 eval `scramv1 runtime -sh`
