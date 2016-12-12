8065991ea635a63ab233099721075cc5e3f21f09
Charlie <charles.mueller@cern.ch>
2016-12-12 16:13:43 -0500

    updating condor submit scripts

--- TemplateMakers/test/condor_test/hadd_many.sh
+++ TemplateMakers/test/condor_test/hadd_many.sh
@@ -2,6 +2,7 @@
 #usage: ./hadd_many.sh
 
 #samples=( "tth_powheg_jetClean_test" "ttbar_semiLep_jetClean_test" "ttw_jetClean_test" )
+#samples=( "ttbar_semiLep_jetClean_test" "ttw_jetClean_test" )
 samples=( "tth_training_2lssos" "ttbar_training_2lss" )
 #samples=( "tth_powheg_genFilterTraining" "ttbar_semiLep_genFilterTraining" "ttw_genFilterTraining" )
 #samples=( "tth_aMC_old" "ttbar_semiLep_powheg" "ttbar_diLep_mg" "ttW_aMCatNLO" "ttZ_aMCatNLO" "ttGammaStar" "tttt" "tZq" "WW_diboson" "ZZ_diboson" "WZ_diboson" "WWqq_rares" "WW_doublescatering_rares" "WWZ_rares" "WZZ_rares" "ZZZ_rares" ) 
