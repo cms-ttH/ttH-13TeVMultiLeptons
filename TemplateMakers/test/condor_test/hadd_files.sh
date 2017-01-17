9dc724331da5d28b2842008d28ee432164f7a01f
Charlie <charles.mueller@cern.ch>
2017-01-17 11:21:26 -0500

    misc updates to condor scripts

--- TemplateMakers/test/condor_test/hadd_files.sh
+++ TemplateMakers/test/condor_test/hadd_files.sh
@@ -4,19 +4,14 @@
 
 prefix=$1
 
-#target_dir="/afs/crc.nd.edu/user/c/cmuelle2/CMSSW_8_0_14/src/ttH-13TeVMultiLeptons/TemplateMakers/test/reco_bdt/nov21_genFilterTraining"
-#target_dir="/afs/crc.nd.edu/user/c/cmuelle2/CMSSW_8_0_14/src/ttH-13TeVMultiLeptons/TemplateMakers/test/selection_trees/condor"
-#target_dir="/scratch365/cmuelle2/selection_trees/nov22_ICHEP_trees"
-#target_dir="/scratch365/cmuelle2/extraction_trees/nov22_ICHEP_trees"
-target_dir="/scratch365/cmuelle2/bdt_test/"
-#target_dir="/scratch365/cmuelle2/extraction_trees/nov22_ICHEP_trees_with_WgtHist"
+target_dir="/scratch365/cmuelle2/extraction_trees/jan15_ICHEP_trees_withFactorizedRecoBdt_fastPerm_yesHiggsLoop_genFilterWeights/";
 
 files="$target_dir/$prefix*.root" 
 
 #postfix="_genFilterTraining_2lss.root"
-#postfix="_2lss_extraction.root"
+postfix="_2lss_extraction.root"
 #postfix="_2lss_selection.root"
-postfix="_2lss_trainingSelection.root"
+#postfix="_2lss_trainingSelection.root"
 output="$target_dir/../$prefix$postfix"
 
 export SCRAM_ARCH=slc6_amd64_gcc530
--- TemplateMakers/test/condor_test/hadd_many.sh
+++ TemplateMakers/test/condor_test/hadd_many.sh
@@ -3,9 +3,9 @@
 
 #samples=( "tth_powheg_jetClean_test" "ttbar_semiLep_jetClean_test" "ttw_jetClean_test" )
 #samples=( "ttbar_semiLep_jetClean_test" "ttw_jetClean_test" )
-samples=( "tth_training_2lssos" "ttbar_training_2lss" )
+#samples=( "tth_sigExtr_training_2lss" "ttbar_sigExtr_training_2lss" )
 #samples=( "tth_powheg_genFilterTraining" "ttbar_semiLep_genFilterTraining" "ttw_genFilterTraining" )
-#samples=( "tth_aMC_old" "ttbar_semiLep_powheg" "ttbar_diLep_mg" "ttW_aMCatNLO" "ttZ_aMCatNLO" "ttGammaStar" "tttt" "tZq" "WW_diboson" "ZZ_diboson" "WZ_diboson" "WWqq_rares" "WW_doublescatering_rares" "WWZ_rares" "WZZ_rares" "ZZZ_rares" ) 
+samples=( "tth_aMC_old" "ttbar_semiLep_powheg" "ttbar_diLep_mg" "ttW_aMCatNLO" "ttZ_aMCatNLO" "ttGammaStar" "tttt" "tZq" "WW_diboson" "ZZ_diboson" "WZ_diboson" "WWqq_rares" "WW_doublescatering_rares" "WWZ_rares" "WZZ_rares" "ZZZ_rares" ) 
 
 for i in "${samples[@]}"
 do
