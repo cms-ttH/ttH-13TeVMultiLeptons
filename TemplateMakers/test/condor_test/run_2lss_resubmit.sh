c7c9eedc3a2954a1fb5e273ca359f2daf461ac9e
Charlie <charles.mueller@cern.ch>
2017-05-12 14:56:34 -0400

    improved condor resubmission

--- TemplateMakers/test/condor_test/run_2lss_resubmit.sh
+++ TemplateMakers/test/condor_test/run_2lss_resubmit.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 
-sed -i 's/^sample = .*/sample = data/' run_2lss_resubmit.submit
-for job in 179 85 132
-do
-    sed -i 's/^job_no = .*/job_no = '$job'/' run_2lss_resubmit.submit
-    condor_submit run_2lss_resubmit.submit
-done
+samples=('tZq:54,75,77' 'ZZ_diboson:18' 'ttZ_aMCatNLO:4')
 
+# Loop over samples
+for sample in ${samples[@]}
+do
+    # delete previous array/list (this is crucial!)
+    unset jobs
+    # split sub-list if available
+    if [[ $sample == *":"* ]]
+    then
+        # split sample name from sub-list
+        tmpArray=(${sample//:/ })
+        sample=${tmpArray[0]}
+        jobs=${tmpArray[1]}
+        # make array from simple string
+        jobs=(${jobs//,/ })
+    fi
+
+    sed -i 's/^sample = .*/sample = '$sample'/' run_2lss_resubmit.submit
+
+    # Loop over jobs
+    for job in ${jobs[@]}
+    do
+	sed -i 's/^job_no = .*/job_no = '$job'/' run_2lss_resubmit.submit
+	condor_submit run_2lss_resubmit.submit
+    done
+done
