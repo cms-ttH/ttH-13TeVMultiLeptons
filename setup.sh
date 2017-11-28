44b1f3d6f0bbd3a72767d58d5979e99af7b8b623
geoff-smith <geoff127@gmail.com>
2017-11-28 16:35:21 +0100

    update to CMSSW_9_4_0

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 cat <<EOF
 ========================================================================
 This script is creating a working directory for ntuplizing and plotting
-the ttH→multilep analysis in 80X
+the ttH→multilep analysis in 94X
 Output is in setup.log
 ========================================================================
 EOF
@@ -13,10 +13,8 @@ EOF
    set -o xtrace
 
    export SCRAM_ARCH=slc6_amd64_gcc630
-#   scramv1 project CMSSW CMSSW_8_0_26_patch1
-#   cd CMSSW_8_0_26_patch1/src
-   scramv1 project CMSSW CMSSW_9_4_0_pre3
-   cd CMSSW_9_4_0_pre3/src
+   scramv1 project CMSSW CMSSW_9_4_0
+   cd CMSSW_9_4_0/src
    set +o xtrace
    eval $(scramv1 runtime -sh)
    set -o xtrace
@@ -32,7 +30,6 @@ EOF
    git clone -b cmssw940pre3 git@github.com:cms-ttH/ttH-LeptonID.git ttH/LeptonID
    git clone git@github.com:cms-ttH/ttH-13TeVMultiLeptons.git
    
-#   sed -i 's|Math/include|Math/interface|' CommonTools/Utils/interface/normalizedPhi.h
 
    curl --create-dirs -o LLR/NtupleProducer/plugins/MuonRefPruner.cc https://raw.githubusercontent.com/LLRCMS/LLRHiggsTauTau/6d4d486beb11efc85d4d3d4184c4e00e85c1261f/NtupleProducer/plugins/MuonRefPruner.cc
    cat <<EOB >LLR/NtupleProducer/plugins/BuildFile.xml
@@ -46,8 +43,6 @@ EOB
 
    scram b -j 32
 
-   cd $CMSSW_BASE/external/$SCRAM_ARCH/
-   git clone -b egm_id_80X_v1 https://github.com/ikrav/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data
 ) > setup.log
 
 
