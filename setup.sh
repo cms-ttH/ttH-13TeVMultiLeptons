c9ceba6c89ed4ec79c2415953a6c99662c350623
geoff-smith <geoff127@gmail.com>
2017-12-07 15:24:04 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 cat <<EOF
 ========================================================================
 This script is creating a working directory for ntuplizing and plotting
-the ttH→multilep analysis in 80X
+the EFT multilepton analysis in 94X
 Output is in setup.log
 ========================================================================
 EOF
@@ -12,25 +12,22 @@ EOF
    set -e
    set -o xtrace
 
-   export SCRAM_ARCH=slc6_amd64_gcc530
-   scramv1 project CMSSW CMSSW_8_0_26_patch1
-   cd CMSSW_8_0_26_patch1/src
+   export SCRAM_ARCH=slc6_amd64_gcc630
+   scramv1 project CMSSW CMSSW_9_4_0
+   cd CMSSW_9_4_0/src
    set +o xtrace
    eval $(scramv1 runtime -sh)
    set -o xtrace
 
-   git cms-addpkg CommonTools/Utils
    git cms-init > /dev/null
 
-   git cms-merge-topic cms-met:METRecipe_8020
-   git cms-merge-topic ikrav:egm_id_80X_v2
-   git cms-merge-topic gpetruc:badMuonFilters_80X_v2
+#   git cms-merge-topic cms-met:METRecipe_8020
+#   git cms-merge-topic ikrav:egm_id_80X_v2
+#   git cms-merge-topic gpetruc:badMuonFilters_80X_v2
 
-   git clone -b multilep_m17 git@github.com:cms-ttH/MiniAOD.git
-   git clone -b multilep_m17 git@github.com:cms-ttH/ttH-LeptonID.git ttH/LeptonID
-   git clone -b EFT_code git@github.com:cms-ttH/ttH-13TeVMultiLeptons.git
-   
-   sed -i 's|Math/include|Math/interface|' CommonTools/Utils/interface/normalizedPhi.h
+   git clone -b cmssw940pre3 git@github.com:cms-ttH/MiniAOD.git
+   git clone -b EFT_code git@github.com:cms-ttH/ttH-LeptonID.git ttH/LeptonID
+   git clone -b EFT_code git@github.com:cms-ttH/ttH-13TeVMultiLeptons.git   
 
    curl --create-dirs -o LLR/NtupleProducer/plugins/MuonRefPruner.cc https://raw.githubusercontent.com/LLRCMS/LLRHiggsTauTau/6d4d486beb11efc85d4d3d4184c4e00e85c1261f/NtupleProducer/plugins/MuonRefPruner.cc
    cat <<EOB >LLR/NtupleProducer/plugins/BuildFile.xml
@@ -44,8 +41,6 @@ EOB
 
    scram b -j 8
 
-   cd $CMSSW_BASE/external/$SCRAM_ARCH/
-   git clone -b egm_id_80X_v1 https://github.com/ikrav/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data
 ) > setup.log
 
 
